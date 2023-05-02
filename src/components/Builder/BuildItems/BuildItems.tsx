09a95c8dec23c391219a0e581bce813bbba97bd5
Charles Finnestad <cfinnestad@gmail.com>
2023-05-02 11:59:17 -0700

    Fixes

---
+++ src/components/Builder/BuildItems/BuildItems.tsx
@@ -0,0 +1,12 @@
+import { Options } from "../../Actions/Actions"
+
+export interface BuildProps {
+    Items: Item[],
+    ItemTypes: object,
+    Options: Options
+}
+const BuildItems = ({Items, ItemTypes, Options}: BuildProps) => {
+    return <></>
+}
+
+export default BuildItems
