caa48a736668a6e99006d0e4d18e2b7b17ca3542
Chuck Finnestad <CFinnestad@gci.com>
2023-05-04 11:09:10 -0700

    changes

--- src/components/Items/ShowItems.tsx
+++ src/components/Items/ShowItems.tsx
@@ -40,7 +40,7 @@ const Handle = styled.div`
 
 const ShowItem = ({ItemProps, index}: ShowItemProps) => {
     // @ts-ignore
-    return <Draggable type='Item' key={ItemProps.Item.id}  draggableId={ItemProps.Item.id} index={index}>
+    return <Draggable key={index} type='Item' draggableId={ItemProps.Item.id} index={index}>
         {(providedDraggable: DraggableProvided, snapshotDraggable:DraggableStateSnapshot) => (
             <Card
                 ref={providedDraggable.innerRef}
@@ -59,7 +59,7 @@ const ShowItem = ({ItemProps, index}: ShowItemProps) => {
                         height="24"
                         viewBox="0 0 24 24">
                         <path
-                            fill="currentColor"
+                            // fill="currentColor"
                             d="M3,15H21V13H3V15M3,19H21V17H3V19M3,11H21V9H3V11M3,5V7H21V5H3Z"
                         />
                     </svg>
