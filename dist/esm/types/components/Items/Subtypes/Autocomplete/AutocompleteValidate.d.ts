3d3ce54391d7758e34361c8bc7f7d96f49e5ca6b
Chuck Finnestad <CFinnestad@gci.com>
2023-06-20 09:56:21 -0700

    Publish Changes

--- dist/esm/types/components/Items/Subtypes/Autocomplete/AutocompleteValidate.d.ts
+++ dist/esm/types/components/Items/Subtypes/Autocomplete/AutocompleteValidate.d.ts
@@ -1,4 +1,4 @@
 import { FieldItem } from "../../Items";
 import { Options } from "../../../Builder";
-declare const ServiceCityValidate: (item: FieldItem, options: Options) => boolean;
-export default ServiceCityValidate;
+declare const AutocompleteValidate: (item: FieldItem, options: Options) => boolean;
+export default AutocompleteValidate;
