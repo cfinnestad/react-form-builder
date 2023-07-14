0ef4aa49c9354b9e2d20a407b06f13661bb6c2d6
Peter Marsh <PMarsh@gci.com>
2023-07-14 13:41:04 -0800

    Added submit edit field

--- src/components/Items/Subtypes/Select/SelectEdit.tsx
+++ src/components/Items/Subtypes/Select/SelectEdit.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import {FieldProps, isSelect} from "../../Items";
+import Options, {SelectedType} from "../../../Options/Options";
 
-const SelectEdit = () => {
-    return (
-        <></>
-    );
+const SelectEdit = ({item, options}: FieldProps) => {
+    if (!isSelect(item)) return <></>
+
+    return <>
+        <Options useMultiples={true} item={item} options={options} selectedType={item.multiples ? SelectedType.Multiple : SelectedType.Single}/>
+    </>
 }
 
 export default SelectEdit;
