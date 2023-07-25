ba4fc378b8f75c0712b6a082dc3eaf578e2318dd
Peter Marsh <PMarsh@gci.com>
2023-07-25 14:44:07 -0800

    Publishing package update

--- dist/cjs/types/components/Items/Subtypes/Boolean/BooleanEdit.d.ts
+++ dist/cjs/types/components/Items/Subtypes/Boolean/BooleanEdit.d.ts
@@ -1,4 +1,4 @@
 import React from "react";
 import { BooleanProps } from "../../Items";
-declare const BooleanEdit: (fieldProps: BooleanProps) => React.JSX.Element;
+declare const BooleanEdit: ({ item, options }: BooleanProps) => React.JSX.Element;
 export default BooleanEdit;
