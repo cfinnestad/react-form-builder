68a7042e5d6b0de39646d002f6eb61cc2360088d
Tommy Johnson <tjohnson@gci.com>
2023-08-11 14:42:38 -0800

    Add edit capability for number subtype

--- src/components/Items/Subtypes/Number/NumberEdit.tsx
+++ src/components/Items/Subtypes/Number/NumberEdit.tsx
@@ -1,131 +1,126 @@
-import React, {ChangeEvent, Dispatch, SetStateAction, useState} from "react";
-import {FieldItem, FieldProps, NumberSubtype} from "../../Items";
-// import {TextField, Checkbox, FormGroup, FormControlLabel} from "@mui/material";
-// import ShowErrors from "../ShowErrors";
-// import subtypeEdit from "../SubtypeEdit";
+import React, { ChangeEvent, useState } from "react";
+import { NumberProps } from "../../Items";
+import ShowErrors from "../ShowErrors";
+import { Stack, TextField } from "@mui/material";
 
-export const NumberEdit = ({item, items, options}: FieldProps ) => {
-    // const [valueError, setValueError] = useState( false)
-    // const [valueErrors, setValueErrors] = useState( [] as string[])
-    // const [minLengthError, setMinLengthError] = useState( false)
-    // const [minLengthErrors, setMinLengthErrors] = useState( [] as string[])
-    // const [maxLengthError, setMaxLengthError] = useState( false)
-    // const [maxLengthErrors, setMaxLengthErrors] = useState( [] as string[])
-    //
-    // const onChangeMinLength = (event: ChangeEvent<HTMLInputElement>) => {
-    //
-    //     const value = event.target.value === '' ? undefined : parseInt(event.target.value)
-    //
-    //     if (value !== undefined) {
-    //         if (value < 0) {
-    //             setMinLengthError(true)
-    //             setMaxLengthErrors(['min must be a positive number'])
-    //             return false
-    //         }
-    //         if (subtype.max && (value > subtype.max)) {
-    //             setMinLengthError(true)
-    //             setMaxLengthErrors(['Min must not be greater than Max Length'])
-    //             return false
-    //         }
-    //     }
-    //     setMinLengthError(false)
-    //     setMinLengthErrors([]);
-    //     subtype.min = value
-    //     setSubtype(subtype)
-    // }
-    //
-    // const onChangeMaxLength = (event: ChangeEvent<HTMLInputElement>) => {
-    //     const value = event.target.value === '' ? undefined : parseInt(event.target.value)
-    //     if (value !== undefined) {
-    //         if (value < 1) {
-    //             setMaxLengthError(true)
-    //             setMaxLengthErrors(['Max Length must be greater the 0'])
-    //             return false
-    //         }
-    //         if (subtype.min && (value < subtype.min)) {
-    //             setMaxLengthError(true)
-    //             setMaxLengthErrors(['Max Length must not be less than Min Length '])
-    //             return false
-    //         }
-    //     }
-    //     setMaxLengthError(false);
-    //     setMaxLengthErrors([]);
-    //     subtype.max = value
-    //     setSubtype(subtype)
-    // }
-    //
-    // const onChangeValue = (event: ChangeEvent<HTMLInputElement>) => {
-    //     const value = event.target.value || undefined
-    //     console.log('typeof value...', typeof value )
-    //     const st = {...subtype}
-    //     if (value) {
-    //         if (value.length < ((item.subtype as NumberSubtype).min || 0)) {
-    //             setValueError(true)
-    //             setValueErrors([item.label + ' must be at least ' + (item.subtype as NumberSubtype).min + 'charters long'])
-    //             return
-    //         }
-    //         if (value.length > ((item.subtype as NumberSubtype).max || 0)) {
-    //             setValueError(true)
-    //             setValueErrors([item.label + ' cannot exceed ' + (item.subtype as NumberSubtype).max + 'charters'])
-    //             return
-    //         }
-    //         st.value = Number.isInteger(value) ? Number.parseInt(value) : Number.parseFloat(value)
-    //     } else {
-    //         delete st.value
-    //     }
-    //     setSubtype(st)
-    // }
-    //
-    // // const onClickMultiline = () => {
-    // //     const st = {...subtype}
-    // //     if (subtype.multiline !== undefined) {
-    // //         delete st.multiline
-    // //     } else {
-    // //         st.multiline = true
-    // //     }
-    // //     setSubtype(st)
-    // //
-    // // }
-    //
-    // return <>
-    //     <TextField
-    //         size='small'
-    //         fullWidth={true}
-    //         label='Value'
-    //         type="number"
-    //         error={valueError}
-    //         value={subtype.value}
-    //         onChange={onChangeValue}
-    //     />
-    //     <ShowErrors errors={valueErrors}/>
-    //     {/*<FormGroup>*/}
-    //     {/*    <FormControlLabel control={<Checkbox checked={subtype.multiline || false} onClick={onClickMultiline}/>} label="Multiline" labelPlacement="start"/>*/}
-    //     {/*</FormGroup>*/}
-    //     <TextField
-    //         size='small'
-    //         fullWidth={true}
-    //         label='Min Length'
-    //         type="number"
-    //         inputProps={{"min": (subtype.max || 0)}}
-    //         error={minLengthError}
-    //         value={subtype.min}
-    //         onChange={onChangeMinLength}
-    //     />
-    //     <ShowErrors errors={minLengthErrors}/>
-    //     <TextField
-    //         size='small'
-    //         fullWidth={true}
-    //         label='Max Length'
-    //         type="number"
-    //         inputProps={{"min": (subtype.min || 1)}}
-    //         error={maxLengthError}
-    //         value={subtype.max}
-    //         onChange={onChangeMaxLength}
-    //     />
-    //     <ShowErrors errors={maxLengthErrors}/>
-    // </>
-    return <></>
+export const NumberEdit = ({ item, items, options }: NumberProps) => {
+    const [value, setValue] = useState(item.value || "");
+    const [min, setMin] = useState(item.min || null);
+    const [max, setMax] = useState(item.max || null);
+    const [valueError, setValueError] = useState([] as string[]);
+    const [minError, setMinError] = useState([] as string[]);
+    const [maxError, setMaxError] = useState([] as string[]);
 
+    const handleValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const val = event.target.value;
+        const itm = { ...item, value: val };
+        setValue(val);
+
+        if (val && isNaN(+val))
+            setValueError(["Value must be a number!"]);
+        else if (min && (parseFloat(val) < min))
+            setValueError(["Value cannot be less than the Min Value setting!"]);
+        else if (max && (parseFloat(val) > max))
+            setValueError(["Value cannot be greater than the Max Value setting!"]);
+        else {
+            setValueError([]);
+            options.SetItem(itm);
+        }
+    };
+
+    const handleMaxValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const val = parseInt(event.target.value);
+        const itm = { ...item, max: val };
+        setMax(val);
+
+        if (val && isNaN(+val))
+            setMaxError(["Max Value must be an integer!"]);
+        else if (val === 0)
+            setMaxError(["Max Value must be greater than zero!"]);
+        else if (min && (val < min))
+            setMaxError(["Max Value must not be less than Min Value!"]);
+        else {
+            setMaxError([]);
+
+            // Make sure the Value field still complies with the new Min Value setting
+            if (value && parseFloat(value.toString()) > val)
+                setValueError(["Value cannot be less than the Min Value setting!"]);
+            else
+                setValueError([]);
+
+            options.SetItem(itm);
+        }
+    };
+
+    const handleMinValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const val = parseInt(event.target.value);
+        const itm = { ...item, min: val };
+        setMin(val);
+
+        if (val && isNaN(+val))
+            setMinError(["Min Value must be an integer!"]);
+        else if (val === 0)
+            setMinError(["Min Value must be greater than zero!"]);
+        else if (max && (val > max))
+            setMinError(["Min Value must not be greater than Max Value!"]);
+        else {
+            setMinError([]);
+
+            // Make sure the Value field still complies with the new Min Value setting
+            if (value && parseFloat(value.toString()) < val)
+                setValueError(["Value cannot be less than the Min Value setting!"]);
+            else
+                setValueError([]);
+
+            options.SetItem(itm);
+        }
+    };
+
+    return (
+        <>
+            <Stack spacing={1}>
+                <TextField
+                    error={valueError.length > 0 ? true : false}
+                    id={`${item.id}-field-value`}
+                    inputProps={{ pattern: '\d*' }}
+                    label="Value"
+                    onChange={handleValueChange}
+                    size="small"
+                    type="text"
+                    value={value}
+                />
+                <ShowErrors errors={valueError} />
+            </Stack>
+
+            <Stack spacing={1}>
+                <TextField
+                    error={minError.length > 0 ? true : false}
+                    id={`${item.id}-min-value`}
+                    inputProps={{ min: 1, shrink: true }}
+                    label="Min Value"
+                    onChange={handleMinValueChange}
+                    size="small"
+                    type="number"
+                    value={min}
+                />
+                <ShowErrors errors={minError} />
+            </Stack>
+
+            <Stack spacing={1}>
+                <TextField
+                    error={maxError.length > 0 ? true : false}
+                    id={`${item.id}-max-value`}
+                    inputProps={{ min: 1, shrink: true }}
+                    label="Max Value"
+                    onChange={handleMaxValueChange}
+                    size="small"
+                    type="number"
+                    value={max}
+                />
+                <ShowErrors errors={maxError} />
+            </Stack>
+        </>
+    );
 }
 
-export default NumberEdit
+export default NumberEdit;
