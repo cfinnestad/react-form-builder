75861bb2589fea990bf89a99efdc52514214bb5e
Chuck Finnestad <CFinnestad@gci.com>
2023-08-22 16:28:26 -0800

    DND Fixes to click n click

--- src/components/Items/Group/ItemGroup.tsx
+++ src/components/Items/Group/ItemGroup.tsx
@@ -2,22 +2,16 @@ import React from "react";
 import {GroupProps} from "../Items";
 import {SortableContext, verticalListSortingStrategy} from "@dnd-kit/sortable";
 import ShowItem from "../ShowItem";
-import {Box, List, ListItem, Stack, Typography} from "@mui/material";
-import DensitySmallIcon from '@mui/icons-material/DensitySmall';
-import {activeStyle} from "../../Builder/Builder";
+import {List, ListItem, Stack, Typography} from "@mui/material";
 
-const ItemGroup = ({item, items, options, errorHandler}: GroupProps) => {
+const ItemGroup = ({item, items, options, activeItem, setActiveItem, errorHandler}: GroupProps) => {
     if (options.IsBuild) {
         return <>
             <Typography variant="h5">{item.label}</Typography>
             <SortableContext
                 items={item.items.map(item => item.id)}
                 strategy={verticalListSortingStrategy}>
-                {item.items.map((item) => <ShowItem key={item.id} item={item} items={items} options={options} errorHandler={errorHandler} />)}
-                items={item.items}
-                strategy={verticalListSortingStrategy}
-            >
-                {item.items.map((itm) => <ShowItem key={itm.id} item={itm} items={items} activeItem={activeItem} setActiveItem={setActiveItem} groupId={item.id} options={options}/>)}
+                {item.items.map((itm) => <ShowItem key={itm.id} item={itm} items={items} activeItem={activeItem} setActiveItem={setActiveItem} errorHandler={errorHandler} groupId={item.id} options={options}/>)}
             </SortableContext>
         </>
     }
