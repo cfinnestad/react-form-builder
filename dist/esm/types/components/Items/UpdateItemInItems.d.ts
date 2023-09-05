eedefe18411fbbe6799283d3e72fc48cf74a0d49
Chuck Finnestad <CFinnestad@gci.com>
2023-09-05 15:32:31 -0700

    Publish Changes

--- dist/esm/types/components/Items/UpdateItemInItems.d.ts
+++ dist/esm/types/components/Items/UpdateItemInItems.d.ts
@@ -1,3 +1,3 @@
 import { AnyItem } from "./Items";
-declare const UpdateItemInItems: (item: AnyItem, items: AnyItem[], prefix?: string) => AnyItem[];
+declare const UpdateItemInItems: (item: AnyItem, items: AnyItem[], prefix?: string) => void;
 export default UpdateItemInItems;
