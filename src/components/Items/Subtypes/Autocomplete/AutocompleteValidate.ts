f546c15608e5b8baf1443a84206816592cdcfe42
Chuck Finnestad <CFinnestad@gci.com>
2023-09-14 16:48:15 -0700

    Fix for Autocomplete validating when not required

--- src/components/Items/Subtypes/Autocomplete/AutocompleteValidate.ts
+++ src/components/Items/Subtypes/Autocomplete/AutocompleteValidate.ts
@@ -12,7 +12,7 @@ const AutocompleteValidate = (item: AutocompleteSubtype, options: Options): bool
     item.errorText = undefined
     if(item.required && !item.value) {
         item.errorText = options.getError('required', item)
-    } else if (!item.value && !item.allowAnyInput && item.options.filter(option => option.value?.toLowerCase() === item.value?.toLowerCase() || option.label?.toLowerCase() === item.value?.toLowerCase()).length === 0 ) {
+    } else if (item.value && !item.allowAnyInput && item.options.filter(option => option.value?.toLowerCase() === item.value?.toLowerCase() || option.label?.toLowerCase() === item.value?.toLowerCase()).length === 0 ) {
         item.errorText = options.getError('invalidSelection', item)
     }
     if(item.errorText === undefined) {
