9f5ebdde114c0218c363811e877a338e0a55f29e
Chuck Finnestad <CFinnestad@gci.com>
2024-02-12 11:45:17 -0900

    SubmitColor Fixes

--- dist/esm/types/shared/themes/TestTheme.d.ts
+++ dist/esm/types/shared/themes/TestTheme.d.ts
@@ -1,1 +1,14 @@
 export declare const TestTheme: import("@mui/material").Theme;
+declare module '@mui/material/styles' {
+    interface Palette {
+        custom: Palette['primary'];
+    }
+    interface PaletteOptions {
+        custom: PaletteOptions['primary'];
+    }
+}
+declare module '@mui/material/Button' {
+    interface ButtonPropsColorOverrides {
+        custom: true;
+    }
+}
