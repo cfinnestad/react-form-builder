26f51dbeacb496bce44df7e88644c7b34df8d1a4
CFinnestad <cfinnestad@gci.com>
2024-06-20 08:54:08 -0800

    publish changes

--- dist/esm/types/components/Items/Subtypes/File/FileTypes.d.ts
+++ dist/esm/types/components/Items/Subtypes/File/FileTypes.d.ts
@@ -1,5 +1,6 @@
 import { Accept } from "react-dropzone";
+import { Options } from "../../Items";
 export declare const FileTypes: Accept;
-export declare const getFilesTypes: (types: string[] | undefined) => Accept | undefined;
-export declare const isValidType: (type: string) => boolean;
-export declare const getExtensions: (fileTypes: string[]) => string[];
+export declare const getFilesTypes: (types: string[] | undefined, options: Options) => Accept | undefined;
+export declare const isValidType: (type: string, options: Options) => boolean;
+export declare const getExtensions: (fileTypes: string[], options: Options) => string[];
